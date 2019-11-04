5180fc62e561f05b122006b0ff8264428e8e6d95
evan holt <adiga_knight@hotmail.com>
2019-11-04 20:53:24 +0200

    Database connection test & dotenv setup

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CONNECT,{useNewUrlParser:true,useUnifiedTopology: true });
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price:Number
+});
+
+const Product = mongoose.model('Product',ProductSchema);
 
 app.get('/',(req,res)=> {
-  res.send("Test Works");
+  const product = new Product({
+    name:"Test",
+    price:5
+  });
+  product.save()
+  .then(()=> {
+    res.send("Works.Check DB");
+  });
 })
 
 const PORT = process.env.PORT || 5000;
