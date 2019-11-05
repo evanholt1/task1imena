3907f1903ac167b86f8c0c1487e11c550ba16471
evan holt <adiga_knight@hotmail.com>
2019-11-05 18:08:01 +0200

    seperated Product Model & added a promise to .connect()

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
+// npm imports
 const express = require('express')
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// file imports
+
 const app = express();
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_CONNECT,{useNewUrlParser:true,useUnifiedTopology: true });
 
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price:Number
-});
 
-const Product = mongoose.model('Product',ProductSchema);
-
-app.get('/',(req,res)=> {
-  const product = new Product({
-    name:"Test",
-    price:5
+mongoose.connect(process.env.MONGO_CONNECT,{useNewUrlParser:true,useUnifiedTopology: true })
+.then(()=>{
+  console.log("Database Connected");
+  app.get('/',(req,res)=> {
+    // const product = new Product({
+    //   name:"Test",
+    //   price:5
+    // });
+    // product.save()
+    // .then(()=> {
+    //   res.send("Works.Check DB");
+    // });
+    res.send("HERE");
   });
-  product.save()
-  .then(()=> {
-    res.send("Works.Check DB");
-  });
+  
+  const PORT = process.env.PORT || 5000;
+  
+  app.listen(PORT,console.log(`Server started on port ${PORT}`));
 })
+.catch(err=>res.send("Database Error Occured. Please Try Again Later."));
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`Server started on port ${PORT}`));
+
